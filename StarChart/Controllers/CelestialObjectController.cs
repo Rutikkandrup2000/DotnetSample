3385542a9df1623da4d382d4eb1a193b827e11fe
Redmi7Note <snehalbufade@gmail.com>
2022-09-23 15:35:59 +0530

    CelestialObjectController.cs

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -40,5 +40,16 @@ namespace StarChart.Controllers
             }
             return Ok(celestialObjects);
         }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var celestialObjects = _context.CelestialObjects.ToList();
+            foreach (var celestialObject in celestialObjects)
+            {
+                celestialObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == celestialObject.Id).ToList();
+            }
+            return Ok(celestialObjects);
+        }
     }
 }
